acc97e9ad93ebb92d589c9d5b4f586098592be8a
Ralitsa-Djurkova <74554806+Ralitsa-Djurkova@users.noreply.github.com>
2022-05-09 12:40:03 +0300

    Add folder with the solution

---
+++ Summator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Summator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum = Summator.Sum(new int[] { 1, 2, 3, 4, 5, 6 });
+
+            var input = Console.ReadLine();
+           
+            if(sum == 21)
+            {
+                Console.WriteLine("TEST PASS");
+            }
+            else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+
+            double average = Summator.Average(new double[] { 5, 10, 15 });
+
+            if(average == 10)
+            {
+                Console.WriteLine("TEST PASS");
+            }
+            else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+        }
+    }
+}
